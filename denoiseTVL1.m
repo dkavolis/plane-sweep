2a38d32c559c405119a202f79c20881fb92719e6
DKavolis <daumantas.kavolis@gmail.com>
2015-08-28 16:21:59 +0100

    denoising using anisotropic diffusion tensor

--- denoiseTVL1.m
+++ denoiseTVL1.m
@@ -1,4 +1,4 @@
-function img = denoiseTVL1(in, lambda, niters, zfar, znear)
+function image = denoiseTVL1(in, img, lambda, niters, zfar, znear)
     L2 = 8;
     tau = 0.02;
     sigma = 1 / L2 / tau;
@@ -20,6 +20,27 @@ function img = denoiseTVL1(in, lambda, niters, zfar, znear)
     div = [0 1 -1]';
     div = gpuArray(div);
     
+%     img = gpuArray(img / 255);
+%     g = exp(- 20 * sqrt(colfilter(img', grad)' .^ 2 + colfilter(img, grad) .^ 2) .^ 0.5);
+    
+    % calculate anisotropic diffusion tensor
+    img = gpuArray(img / 255);
+    dx = colfilter(img', grad)';
+    dy = colfilter(img, grad);
+    d = sqrt(dx .* dx + dy .* dy);
+    dx = dx ./ d;
+    dy = dy ./ d;
+    k = exp(-20 * (d .^ 0.5));
+    T1 = k .* dx .* dx + dy .* dy;
+    T4 = k .* dy .* dy + dx .* dx;
+    T2 = (k - 1) .* dx .* dy;
+    
+    % correct NaNs
+    T1(isnan(T1)) = 1;
+    T4(isnan(T4)) = 1;
+    T2(isnan(T2)) = 0;
+    T3 = T2;
+    
     for i=1:niters
        if i == 1 currsigma = sigma + 1;
        else currsigma = sigma;
@@ -28,8 +49,10 @@ function img = denoiseTVL1(in, lambda, niters, zfar, znear)
        dx = colfilter(X', grad)';
        dy = colfilter(X, grad);
        
-       px = px + currsigma * dx;
-       py = py + currsigma * dy;
+%        px = px + currsigma * dx .* g;
+%        py = py + currsigma * dy .* g;
+       px = px + currsigma * (T1 .* dx + T2 .* dy);
+       py = py + currsigma * (T3 .* dx + T4 .* dy);
        
        l = max(sqrt(px .* px + py .* py), 1);
        px = px ./ l;
@@ -45,5 +68,5 @@ function img = denoiseTVL1(in, lambda, niters, zfar, znear)
     end
     
     X = (zfar - znear) * X + znear;
-    img = gather(X);
+    image = gather(X);
 end
